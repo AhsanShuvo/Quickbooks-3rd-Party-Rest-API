615304a6f6cb1df1edfdf891e945fb0d0639065e
ahsan.habib <ahsan.habib@enosisbd.com>
2020-12-12 00:13:28 +0600

    Add Webhook.

---
+++ QuickbooksApi/Controllers/WebhookController.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace QuickbooksApi.Controllers
+{
+    public class WebhookController : Controller
+    {
+        private string _signature = ConfigurationManager.AppSettings["intuit-signature"];
+        private string _algorithm = ConfigurationManager.AppSettings["algorithm"];
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            var headers = Request.Headers;
+            if (Request.Headers.AllKeys.Contains("Intuit-Signature"))
+            {
+                var value = Request.Headers["Intuit-Signature"];
+            }
+            var payload = Request.RequestType;
+            var code = Request.Files.AllKeys;
+            return View();
+        }
+    }
+}
